9d9232223638b460a54324c70e14edd331b16cca
jfklima <jfranklincontatos@gmail.com>
2022-11-18 19:36:23 -0300

    Instalado o pacote neotree.

--- lisp/projects.el
+++ lisp/projects.el
@@ -6,9 +6,24 @@
   :ensure t
   :diminish projectile-mode
   :config
-  ;; pasta(s) onde o projectile irá buscar port projetos
-  ;; automáticamente na inicialização
   (setq projectile-project-search-path '("~/workspace/projetos/"))
   (projectile-mode +1))
+
+(use-package neotree :ensure t
+  :config
+  (setq neo-theme (if (display-graphic-p) 'icons 'arrow))
+  (setq projectile-switch-project-action 'neotree-projectile-action)
+
+  (let ((map neotree-mode-map))
+    (define-key map (kbd "RET") (kbd "SPC"))
+    (define-key map (kbd "C") 'neotree-create-node)
+    (define-key map (kbd "D") 'neotree-delete-node)
+    (define-key map (kbd "R") 'neotree-rename-node)
+    (define-key map (kbd "P") 'neotree-copy-node))
+
+  (space-leader
+    "d d" 'neotree-toggle
+    "p d" 'neotree-projectile-action
+    ))
 
 (provide 'projects)
