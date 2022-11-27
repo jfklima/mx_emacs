a54c715ee8a50849b3cd14d5729b6839df94a10f
jfklima <jfranklincontatos@gmail.com>
2022-11-27 18:11:06 -0300

    Instalado o color-theme zenburn-theme.

--- lisp/aparence.el
+++ lisp/aparence.el
@@ -5,21 +5,10 @@
 ;;; COLOR-THEME ;;;
 ;;;;;;;;;;;;;;;;;;;
 
-
-(unless (package-installed-p 'doom-themes)
-  (package-install 'doom-themes))
-
-(require 'doom-themes)
-
-;; Load the theme (doom-one, doom-molokai, etc); keep in mind that each
-;; theme may have their own settings.
-(load-theme 'doom-monokai-classic t)
-
-;; Enable flashing mode-line on errors
-(doom-themes-visual-bell-config)
-
-;; Enable custom neotree theme
-(doom-themes-neotree-config)  ; all-the-icons fonts must be installed!
+(use-package zenburn-theme
+  :ensure t
+  :config
+  (load-theme 'zenburn t))
 
 (use-package doom-modeline
   :ensure t
