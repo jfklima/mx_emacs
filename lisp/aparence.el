d4f7fdfc3f635a9a9f64b86ecc24ad678aaa41d1
jfklima <jfranklincontatos@gmail.com>
2022-12-10 19:56:10 -0300

    Ativado o color-theme dracula do doom-themes.

--- lisp/aparence.el
+++ lisp/aparence.el
@@ -3,13 +3,23 @@
 
 
 (use-package zenburn-theme
+  :disabled t
   :ensure t
   :config
   (load-theme 'zenburn t))
 
 
+(use-package doom-themes
+  :ensure t
+  :config
+  (doom-themes-visual-bell-config)
+  (doom-themes-neotree-config)
+  (load-theme 'doom-dracula t))
+
+
 (use-package doom-modeline
   :ensure t
   :init (doom-modeline-mode 1))
+
 
 (provide 'aparence)
