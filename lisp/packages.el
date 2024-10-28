c4d0da15b18a9b04f3735fb0f7b0740b7bbb20f7
Franklin Lima <jfranklincontatos@gmail.com>
2024-10-28 08:43:41 -0300

    Desativada o opção package-check-signature
    Essa opção estava impedindo de baixar novos pacotes.

--- lisp/packages.el
+++ lisp/packages.el
@@ -1,7 +1,11 @@
 (require 'package)
+
 (setq package-enable-at-startup nil)
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
 (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/"))
+
+(setq package-check-signature nil)
+
 (package-initialize)
 
 (unless (package-installed-p 'use-package)
