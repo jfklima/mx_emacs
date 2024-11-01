b8038d51952e9bf0f385c6ee542639ae5ddcaed6
Franklin Lima <jfranklincontatos@gmail.com>
2024-11-01 20:39:30 -0300

    Ativado o modo recentf
    Agora podemos com o comando "SPC f r" ter uma lista dos arquivos
    visitados recentes para escolher.

--- lisp/util.el
+++ lisp/util.el
@@ -8,4 +8,14 @@
 (use-package beacon :ensure t
   :config (beacon-mode 1))
 
+(use-package recentf
+  :config
+  (setq recentf-max-saved-items 200
+	recentf-max-menu-items 15)
+  (recentf-mode +1))
+
+(space-leader
+  "f r" 'counsel-recentf)
+
+
 (provide 'util)
