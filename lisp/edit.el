48dc2a86543e7e64d4fb106281beac549b0c4ea4
Franklin Lima <jfranklincontatos@gmail.com>
2024-11-01 20:58:02 -0300

    Instalado o plugin text-move
    Agora podemos teclar t ou T no edit-status ou visual-status e assim
    mover o texto na linha ou selecionada para a linha que quisermos.

--- lisp/edit.el
+++ lisp/edit.el
@@ -5,6 +5,7 @@
   (setq completion-ignore-case t)
   (add-hook 'prog-mode-hook #'company-mode))
 
+
 (use-package smartparens
   :ensure t
   :diminish smartparens-mode
@@ -13,6 +14,7 @@
     (require 'smartparens-config)
     (smartparens-global-mode 1)
     (show-paren-mode t)))
+
 
 (use-package crux
   :ensure t
@@ -39,11 +41,31 @@
   (global-set-key [remap undo-redo] 'undo-fu-only-redo))
 
 
-
 (use-package zzz-to-char :ensure t
   :config
   (global-set-key [remap zap-to-char] 'zzz-to-char)
   (global-set-key [remap zap-up-to-char] 'zzz-up-to-char))
+
+
+(use-package move-text :ensure t
+  :preface
+  (defun indent-region-advice (&rest ignored)
+  (let ((deactivate deactivate-mark))
+    (if (region-active-p)
+	(indent-region (region-beginning) (region-end))
+      (indent-region (line-beginning-position) (line-end-position)))
+    (setq deactivate-mark deactivate)))
+
+  :config
+  (advice-add 'move-text-up :after 'indent-region-advice)
+  (advice-add 'move-text-down :after 'indent-region-advice))
+
+(edit-map
+  "t" 'move-text-down
+  "T" 'move-text-up)
+(visual-map
+  "t" 'move-text-down
+  "T" 'move-text-up)
 
 
 (provide 'edit)
