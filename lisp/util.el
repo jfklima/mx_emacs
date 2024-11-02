214064b5e9edda97140667d82cc37cfea9bc4371
Franklin Lima <jfranklincontatos@gmail.com>
2024-11-02 13:36:48 -0300

    Desinstalado o bufler
    O buffler estava dando problemas na linha de modo, e não estava sendo
    muito utilizado.

--- lisp/util.el
+++ lisp/util.el
@@ -1,10 +1,3 @@
-(use-package bufler :ensure t
-  ;; :commands bufler
-  :diminish bufler-mode
-  :config
-  (global-set-key [remap ibuffer] 'bufler)
-  (bufler-mode 1))
-
 (use-package beacon :ensure t
   :config (beacon-mode 1))
 
