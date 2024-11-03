d462f591019e20a09ef6b16da8180d420cee7b86
Franklin Lima <jfranklincontatos@gmail.com>
2024-11-02 22:17:57 -0300

    Configurado um novo centaur-tabs-buffer-groups
    Agora podemos ter projetos e buffers que não pertencem a projetos
    configurados juntos.

--- lisp/util.el
+++ lisp/util.el
@@ -47,6 +47,84 @@
    ;; tenha uma aparência uniforme.
   (centaur-tabs-headline-match)
 
+  (defun centaur-tabs-buffer-groups ()
+  "`centaur-tabs-buffer-groups' control buffers' group rules.
+
+Group centaur-tabs with mode if buffer is derived from `eshell-mode'
+`emacs-lisp-mode' `dired-mode' `org-mode' `magit-mode'.
+All buffer name start with * will group to \"Emacs\".
+Other buffer group by `centaur-tabs-get-group-name' with project name."
+  (list
+   (cond
+
+    ((derived-mode-p 'text-mode)
+     "Editing")
+
+    ((when-let ((project-name (centaur-tabs-project-name)))
+       (format "Project: %s" (projectile-project-name))))
+
+    ((or (string-equal "*" (substring (buffer-name) 0 1))
+         (memq major-mode '( magit-process-mode
+                             magit-status-mode
+                             magit-diff-mode
+                             magit-log-mode
+                             magit-file-mode
+                             magit-blob-mode
+                             magit-blame-mode)))
+     "Common")
+
+    ((derived-mode-p 'config-mode)
+     "Config")
+    ((derived-mode-p 'prog-mode)
+     "Programming")
+    ((derived-mode-p 'dired-mode)
+     "Dired")
+    ((memq major-mode '(helpful-mode
+                        help-mode))
+     "Help")
+    ((memq major-mode '(org-agenda-clockreport-mode
+                        org-src-mode
+                        org-agenda-mode
+                        org-beamer-mode
+                        org-indent-mode
+                        org-bullets-mode
+                        org-cdlatex-mode
+                        org-agenda-log-mode
+                        diary-mode))
+     "OrgMode")
+
+    (t
+     (centaur-tabs-get-group-name (current-buffer))))))
+
+
+  (defun centaur-tabs-hide-tab (x)
+  "Do no to show buffer X in tabs."
+  (let ((name (format "%s" x)))
+    (or
+     ;; Current window is not dedicated window.
+     (window-dedicated-p (selected-window))
+
+     ;; Buffer name not match below blacklist.
+     (string-prefix-p "*epc" name)
+     (string-prefix-p "*helm" name)
+     (string-prefix-p "*Helm" name)
+     (string-prefix-p "*Compile-Log*" name)
+     (string-prefix-p "*lsp" name)
+     (string-prefix-p "*company" name)
+     (string-prefix-p "*Flycheck" name)
+     (string-prefix-p "*tramp" name)
+     (string-prefix-p " *Mini" name)
+     (string-prefix-p "*help" name)
+     (string-prefix-p "*straight" name)
+     (string-prefix-p " *temp" name)
+     (string-prefix-p "*Help" name)
+     (string-prefix-p "*mybuf" name)
+
+     ;; Is not magit buffer.
+     (and (string-prefix-p "magit" name)
+          (not (file-name-extension name)))
+     )))
+
   (setq
    ;; Muda a forma da aba
    centaur-tabs-style "wave"
@@ -63,8 +141,8 @@
    ;; mostra letras, ao invés de números
    centaur-tabs-ace-jump-keys '(?a ?s ?d ?f ?j ?k ?l ?ç))
 
-  (centaur-tabs-group-buffer-groups)
-  (centaur-tabs-group-by-projectile-project)
+  ;; (centaur-tabs-group-buffer-groups)
+  ;; (centaur-tabs-group-by-projectile-project)
 
    (edit-map
      "M-p" 'centaur-tabs-backward
