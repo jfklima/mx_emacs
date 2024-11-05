43a2ac308785266744abc3105b2bfb4cec87e127
Franklin Lima <jfranklincontatos@gmail.com>
2024-11-05 15:46:10 -0300

    Desabilidatdo dash-docs-enable-debugging
    Essa opção ativa mostrava uma tela de debugging quando realizarmos uma
    consulta no counsel-dash ficando a mostra mesmo quando a seleçao do
    elementa já havia sido realizada.

--- lisp/programming.el
+++ lisp/programming.el
@@ -9,10 +9,13 @@
 
 (use-package counsel-dash :ensure t
   :config
+  (setq dash-docs-enable-debugging nil)
   (global-set-key (kbd "C-c i") 'counsel-dash-install-docset)
 
   (mode-leader :keymaps 'prog-mode-map
-  "d" 'counsel-dash))
+    "d" 'counsel-dash
+    "i" 'imenu
+    "." 'counsel-dash-at-point))
 
 
 (add-hook 'python-mode-hook (lambda () (setq-local counsel-dash-docsets '("Python 3"))))
