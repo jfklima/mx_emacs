3499243e53808d0adafb7a4cf3f959582dd17cfb
Franklin Lima <jfranklincontatos@gmail.com>
2024-11-07 19:45:09 -0300

    Troca do ivy pelo helm como interface interativa.
    Porque alguns comando como M-x funcionam melhor no helm do que no ivy.

--- lisp/interface.el
+++ lisp/interface.el
@@ -1,19 +1,51 @@
-(use-package counsel :ensure t
+(use-package helm :ensure t
   :config
-  (setq ivy-use-virtual-buffers t
-	ivy-count-format "(%d/%d) "
-	ivy-height 17
-	ivy-wrap t)
+  (global-set-key (kbd "C-c h") 'helm-command-prefix)
+  (global-unset-key (kbd "C-x c"))
 
-  (define-key ivy-minibuffer-map (kbd "RET") 'ivy-alt-done)
-  (define-key ivy-minibuffer-map (kbd "C-j") 'ivy-immediate-done)
-  (define-key ivy-minibuffer-map (kbd "M-l") 'ivy-backward-kill-word)
+  (define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action)
+  (define-key helm-map (kbd "C-i")  'helm-select-action)
 
-  (ivy-mode +1))
+  (when (executable-find "curl")
+    (setq helm-google-suggest-use-curl-p t))
 
-(space-leader
-    "s s" 'swiper-isearch
-    "s ." 'swiper-isearch-thing-at-point)
+  (setq
+   ;; open helm buffer inside current window, not occupy whole other window
+   helm-split-window-in-side-p           t
+   ;; move to end or beginning of source when reaching top or bottom of source.
+   helm-move-to-line-cycle-in-source     t
+   ;; search for library in `require' and `declare-function' sexp.
+   helm-ff-search-library-in-sexp        t
+   ;; scroll 8 lines other window using M-<next>/M-<prior>
+   helm-scroll-amount                    8
+   helm-ff-file-name-history-use-recentf t
+   helm-echo-input-in-header-line t
+   helm-M-x-reverse-history t)
+
+  (defun spacemacs//helm-hide-minibuffer-maybe ()
+    "Hide minibuffer in Helm session if we use the header line as input field."
+    (when (with-helm-buffer helm-echo-input-in-header-line)
+      (let ((ov (make-overlay (point-min) (point-max) nil nil t)))
+	(overlay-put ov 'window (selected-window))
+	(overlay-put ov 'face
+                     (let ((bg-color (face-background 'default nil)))
+                       `(:background ,bg-color :foreground ,bg-color)))
+	(setq-local cursor-type nil))))
+
+
+  (add-hook 'helm-minibuffer-set-up-hook
+            'spacemacs//helm-hide-minibuffer-maybe)
+
+  (setq helm-autoresize-max-height 50)
+  (setq helm-autoresize-min-height 20)
+
+  (space-leader
+    "SPC" 'helm-M-x
+    "s s" 'helm-occur
+    "f f" 'helm-find-files
+    "b b" 'helm-buffers-list)
+
+  (helm-mode 1))
 
 
 (provide 'interface)
--- lisp/programming.el
+++ lisp/programming.el
@@ -1,16 +1,16 @@
-(use-package counsel-dash :ensure t
+(use-package helm-dash :ensure t
   :config
-  (setq dash-docs-enable-debugging nil)
-  (global-set-key (kbd "C-c i") 'counsel-dash-install-docset)
-
   (mode-leader :keymaps 'prog-mode-map
-    "d" 'counsel-dash
-    "." 'counsel-dash-at-point))
+    "d" 'helm-dash
+    "." 'helm-dash-at-point))
+()
 
+(add-hook 'prog-mode-hook (lambda () (setq-local helm-dash-docsets '("PostgreSQL"))))
 
-(add-hook 'python-mode-hook (lambda () (setq-local counsel-dash-docsets '("Python 3"))))
-(add-hook 'emacs-lisp-mode-hook (lambda () (setq-local counsel-dash-docsets '("Emacs Lisp"))))
-(add-hook 'ruby-mode-hook (lambda () (setq-local counsel-dash-docsets '("Ruby"))))
+(add-hook 'python-mode-hook (lambda () (setq-local helm-dash-docsets '("Python 3"))))
+(add-hook 'emacs-lisp-mode-hook (lambda () (setq-local helm-dash-docsets '("Emacs Lisp"))))
+(add-hook 'ruby-mode-hook (lambda () (setq-local helm-dash-docsets '("Ruby"))))
+
 
 (use-package company-box :ensure t
   :hook (company-mode . company-box-mode))
